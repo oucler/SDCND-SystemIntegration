019558997f9395acdede4df89178191df6afa449
oucler <onurucler@yahoo.com>
2018-01-12 00:05:31 -0800

    This commit includes implementations of: tl_detector.py light_classifier.py waypoint_updater.py GAN model
    The car doesn't stop at the traffic light yet.

---
+++ ros/devel/share/styx_msgs/cmake/styx_msgs-msg-extras.cmake
@@ -0,0 +1,2 @@
+set(styx_msgs_MESSAGE_FILES "/home/student/Classes/Udacity/SDCND/Term3/SDCND-SystemIntegration/ros/src/styx_msgs/msg/TrafficLight.msg;/home/student/Classes/Udacity/SDCND/Term3/SDCND-SystemIntegration/ros/src/styx_msgs/msg/TrafficLightArray.msg;/home/student/Classes/Udacity/SDCND/Term3/SDCND-SystemIntegration/ros/src/styx_msgs/msg/Waypoint.msg;/home/student/Classes/Udacity/SDCND/Term3/SDCND-SystemIntegration/ros/src/styx_msgs/msg/Lane.msg")
+set(styx_msgs_SERVICE_FILES "")
